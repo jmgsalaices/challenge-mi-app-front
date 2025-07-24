ae21a9d40c18b773b88d254cfcc06e939da521c6
J Mauricio G Salaices <51425235+jmgsalaices@users.noreply.github.com>
2025-07-24 00:57:44 -0600

    Generate auth login List Applications structure components, generate login logic services
    Generate List Applications structure components
     Applications List View
          Displays a list of submitted requests (applications).
          Fields shown:
          id
          date
          type (request / offer / complaint)
          status (submitted / completed)
     Create New Application (Request)
          A button labeled "New Application" opens a popup modal for submitting a new request.

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  standalone: true,
 })
 export class Login {
+  loginForm = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
 
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {}
+
+  login() {
+    if (this.loginForm.valid) {
+      this.auth.login(this.loginForm.value).subscribe({
+        next: () => this.router.navigate(['/applications']),
+        error: () => alert('Invalid login'),
+      });
+    }
+  }
 }
